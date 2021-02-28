d1dcf81193afb9213976d179df5e5cd4788e4c9d
hawkar-noori <77577492+hawkar-noori@users.noreply.github.com>
2021-02-28 22:23:49 +0300

    Add files via upload

---
+++ totalnotpayed.php
@@ -0,0 +1,45 @@
+<?php
+
+if(!isset($_SESSION['uNamebnz'])){
+header('Location:login.php');
+$pdo= null;
+
+}
+include "dbcon.php";
+
+$select = $pdo->prepare("SELECT * FROM items WHERE `items`.`sale`>`items`.`payed`  ORDER BY `items`.`id` DESC");
+$select->setFetchMode(PDO::FETCH_ASSOC);
+$select->execute();
+
+  $pdo= null;
+  
+$totalnotpayed=0;
+$totalpayed=0;
+$totalsale=0;
+
+  while($data=$select->fetch()){
+      $notpayed=$data['sale']-$data['payed'];
+ $totalnotpayed=$totalnotpayed+$notpayed;
+ $totalpayed=$totalpayed+$data['payed'];
+
+$totalsale=$totalsale+$data['sale'];
+
+  }
+    ?>
+
+<table class="table table-striped">
+  <thead class="thead-dark">
+  <tr>
+      <th scope="col">Total Not payed</th>
+      <th scope="col">Total payed</th>
+      <th scope="col">total sale</th>
+      </tr>
+  </thead>
+  <tbody>
+  <tr>
+  <td><?php echo $totalnotpayed; ?></td>
+        <td><?php echo $totalpayed; ?></td>      
+        <td><?php echo $totalsale; ?></td>
+        </tr>
+        <tbody>
+        </table>
